743c317ddd2554197164c77c47d656f502f01c1a
rlopes1 <rtaboada@gmail.com>
2023-05-31 11:10:01 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("aplicação está se conectando ao banco de dados")
 console.log ("Enviando para o Github")
 
